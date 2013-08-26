0ec6bc2f2c932700375a10a17c239dae177aa80d
Sergey80 <ses.box@gmail.com>
2013-08-25 21:06:36 -0400

    sample #2 - implicit class

---
+++ src/implicit_test/ImplicitClassTest.scala
@@ -0,0 +1,38 @@
+package implicit_test
+
+/*
+ * Add new method to exiting Int class, That works in particular scope.
+*/
+
+object ImplicitsScope {
+                                        // only one arg argument allowed for 'implicit class'
+  implicit class ExtendedInt(x: Int) {  // does not matter how we call this class. It just defines 'class of methods'
+                                        // x: Int - type 'Int' it does matter - we are adding new methods to "Int"
+
+    def times[A]( f: => A ): Unit = {    // 'times' will be used everywhere where
+      for(i <- 1 to x) {
+        f
+      }
+    }
+
+  }
+
+}
+
+
+object ImplicitClassTest extends App {
+
+  foo1
+
+  def foo1 = {
+    import implicit_test.ImplicitsScope._
+    5 times println("hello")  // now we can use this new 'times()' method for Int class
+  }
+
+  // this will not work, because scope where method "times()" was defined is not imported
+  /*
+  def foo2 = {
+    5 times println("this should not work")
+  }*/
+
+}
