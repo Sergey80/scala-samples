465dc08b45eb117247ff963ebb7ae67dc0b6e9fa
Sergey80 <ses.box@gmail.com>
2013-09-05 14:01:44 -0400

    apply method. example. fix: naming.

--- src/apply_method/ClassWithApplyMethod.scala
+++ src/apply_method/ClassWithApplyMethod.scala
@@ -9,19 +9,19 @@ class ClassWithApplyMethod(x:Int) {
 
 object Starter extends App {
 
-  val x = new ClassWithApplyMethod(1)
+  val obj = new ClassWithApplyMethod(1)
 
 
   println("-1-")
 
-  val y = x(2) // equivalent to x.apply(2)
+  val y = obj(2) // equivalent to x.apply(2)
 
 
   println("-2-")
 
   //val z:Int = x  // this will not lead to apply() evaluation, because we do not use "()" to evaluate it
 
-  val z:Int = x() // equivalent to x.apply()
+  val z:Int = obj() // equivalent to x.apply()
 
   // in this sense apply() method is just default 'empty-name' when you apply "()" on a object
 
