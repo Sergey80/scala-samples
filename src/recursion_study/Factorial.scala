c83eee03cdfcb03539202bfb1aba5f8be28a91a5
Sergey80 <ses.box@gmail.com>
2013-10-09 14:45:51 -0400

    Added two examples that explain Recursion. "Head" vs "Tail" recursion in Scala.

---
+++ src/recursion_study/Factorial.scala
@@ -0,0 +1,43 @@
+package recursion_study
+
+// 1. Understanding recursion
+
+// "Head recursion"
+
+object Factorial extends App {
+
+  def factorial(i:Int) : BigInt = {      // let's take a look if on the call-flow if i = 5
+    if (i  == 0) 1
+    else i * factorial(i-1)           // f(5): 5 * f(4)  // 5 * (4 * ( 3 * (2*1) )
+                                      // f(4): 4* f(3)   // 4 * (3 * (2*1)
+                                      // f(3): 3 * f(2)  // 3 * (2*1)
+                                      // f(2): 2 * f(1)  // 2 * f(1)
+                                      // f(1):  = 1 * f(0) // 1 * 1   // <- here we got "real"/evaluated value "1"
+  }
+
+  // note: "i * " - this is "head" of recursion call
+
+  println ( factorial(1) )
+  println ( factorial(2) )
+  println ( factorial(3) )
+  println ( factorial(4) )
+  println ( factorial(5) )
+
+  // The important thing to understand is:  that function calls are saving into the Stack
+  // and it all these calls are starting evaluating it gets real value.
+  // Then it gets back to from the very end stack call to the beginning, evaluating one after another.
+  // (In our case real value is = 1).
+
+
+  // as you understand, sometimes stack might be overflowed, let's try this:
+
+  println( "factorial(1000): " + factorial(1000) ) // ve...ee...ery big Int
+
+  // but if uncomment this:
+
+  // println( "factorial(9999): " + factorial(9999) )  // java.lang.StackOverflowError
+
+
+  // See "Factorial2" example to see how to use "tail recursion"
+
+}
---
+++ src/recursion_study/Factorial2.scala
@@ -0,0 +1,77 @@
+package recursion_study
+
+import scala.annotation.tailrec
+
+// 2. Understanding recursion
+// "Tail Recursion"
+
+object Factorial2 extends App{
+
+  // the problem with Factorial2 was that we have to keep function call in the stack to get value back to evaluate them
+
+  // i.e. this "i * factorial(i-1)" leads to keep values
+
+  // another words: If we did not have "i * ... ", we may avoid using Stack at all !
+
+  // let's try
+  def factorial(i: Int, result:Int = 1) : BigInt = {  // will stick to same algorithm.
+    if (i == 0) result
+    else factorial(i-1, result = result * i)
+  }
+
+  println( "factorial(5) :" + factorial(5) )
+
+  // As you can see what we do: we just accumulating Result and storing it between function calls - just passing it as argument.
+
+  // So no need to add it to the "head" of recursion call.
+
+  // We just leave recursion call to be LAST expression - this mean "tail recursion"
+
+  // Let's try if it fails with big number as it was before:
+
+  println ( "factorial(9999): " + factorial(9999) ) // No, there is no StackOverflow exception, That's good!
+                              // But we got "0", because BigInt is out of range for our big result.
+                              // This is not our problem for now.
+
+  // Important: Scala support special annotation "@tailrec"
+  // you may put over the function to make sure you use TAIL recursion (it will fail if not)
+
+  // So our function may look like this:
+
+  @tailrec
+  def factorial2(i: Int, result:Int = 1) : BigInt = {
+    if (i == 0) result
+    else factorial2(i-1, result = result * i)
+  }
+
+  println ( "factorial2(5): " + factorial2(5) )
+
+
+  // You many wonder. "Is tail recursion is still recursion if there is not any stack-helping?"
+  //
+  // The answer might be:
+  // for you it "looks like" as recursion, but compiler is able to rewrite/optimize this by using loops (like: "while" or "for")
+
+  // but if you would use while and for it might look not so nice and cool, let's check:
+
+  def factorial3(i:Int) : BigInt = {
+
+    var current = i   // VAR is not cool at all (you know !)
+    var result  = 1
+
+    while(current != 0) {
+      result = result * current
+      current = current - 1
+    }
+
+    result
+
+  }
+
+  println( "factorial3(5): " + factorial3(5) )
+
+  // So, the last example in not recursion at all. And you can see that this code smells: we had to introduce to VARS
+
+
+
+}
