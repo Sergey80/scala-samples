f90ffddf1bd9d1465594539d06d649c87535461f
Sergey80 <ses.box@gmail.com>
2013-10-09 22:15:40 -0400

    Added two examples that explain Recursion. "Head" vs "Tail" recursion in Scala. fixing errors

--- src/recursion_study/Factorial2.scala
+++ src/recursion_study/Factorial2.scala
@@ -7,9 +7,9 @@ import scala.annotation.tailrec
 
 object Factorial2 extends App{
 
-  // the problem with Factorial2 was that we have to keep function call in the stack to get value back to evaluate them
+  // the problem with Factorial2 was that we have to keep function calls in the Stack
 
-  // i.e. this "i * factorial(i-1)" leads to keep values
+  // i.e. this "i * factorial(i-1)" leads to keeping values
 
   // another words: If we did not have "i * ... ", we may avoid using Stack at all !
 
@@ -21,9 +21,9 @@ object Factorial2 extends App{
 
   println( "factorial(5) :" + factorial(5) )
 
-  // As you can see what we do: we just accumulating Result and storing it between function calls - just passing it as argument.
+  // As you can see what we do: we just accumulating Result and storing it between function calls - just passing it as an argument.
 
-  // So no need to add it to the "head" of recursion call.
+  // So no need to add anything to "head" of recursion call.
 
   // We just leave recursion call to be LAST expression - this mean "tail recursion"
 
@@ -33,8 +33,8 @@ object Factorial2 extends App{
                               // But we got "0", because BigInt is out of range for our big result.
                               // This is not our problem for now.
 
-  // Important: Scala support special annotation "@tailrec"
-  // you may put over the function to make sure you use TAIL recursion (it will fail if not)
+  // Important: Scala supports special annotation called "@tailrec"
+  // you may put it over the function to make sure you use TAIL recursion (it will fail if not)
 
   // So our function may look like this:
 
@@ -47,14 +47,15 @@ object Factorial2 extends App{
   println ( "factorial2(5): " + factorial2(5) )
 
 
-  // You many wonder. "Is tail recursion is still "recursion" if there is no any stack-helping?"
+  // You may wonder. "Is tail recursion is still "recursion" if there is no any stack-helping?"
   //
   // The answer might be:
-  // For you it "looks like" as recursion, but compiler is able to rewrite/optimize this by using loops (like: "while" or "for")
+  // For you it "looks like" as recursion (because 'function calls itself'),
+  // but compiler is able to rewrite/optimize this under the hood  by using loops (like: "while" or "for")
 
-  // Just do not forget: you may use recursion calls to implement looping without side-effects (vars).
+  // Just do not forget (from another side): You may use recursion calls to implement looping without side-effects (vars).
 
-  // but if you would use while and for it might look not so nice and cool, let's check:
+  // But if you would use "while" and "for" by your will, it might look not so nice and cool (as with recursion), let's check:
 
   def factorial3(i:Int) : BigInt = {
 
