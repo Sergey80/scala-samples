2677b63a2f5960cc464aeff094d540d91e09c19d
Sergey80 <ses.box@gmail.com>
2013-10-28 22:01:26 -0400

    list. groupBy. make it nicer

--- src/lists/GroupByExample.scala
+++ src/lists/GroupByExample.scala
@@ -9,19 +9,16 @@ object GroupByExample extends App {
   def wordOccurrences(word:Word):Occurrences = {
 
 
-    val groupMap = word.toLowerCase groupBy {  // Map(e -> e, ! -> !, l -> ll, h -> h, o -> o)
+    val groupMap = word groupBy {
       case ch => ch
-    } map {
-      case (k, chars) => (k, chars.length)   // converting
+    } map {   // Map(e -> e, ! -> !, l -> ll, h -> h, o -> o)
+      case (k, chars) => (k, chars.length)   // Map(e -> 1, ! -> 1, l -> 2, h -> 1, o -> 1)
     }
-
-//    println(groupMap)
-//    println(occurrences)
 
     groupMap toList
   }
 
-  val result = wordOccurrences("hello!")
+  val result = wordOccurrences("hello!")  // List((e,1), (!,1), (l,2), (h,1), (o,1))
 
   println("occurrences: " + result)
 
