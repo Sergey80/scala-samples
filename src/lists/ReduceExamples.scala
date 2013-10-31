d719c9f692064e7b423e71fcfba42b5d408d5c49
Sergey80 <ses.box@gmail.com>
2013-10-30 22:38:59 -0400

    #list. reduceLeft. fixed

--- src/lists/ReduceExamples.scala
+++ src/lists/ReduceExamples.scala
@@ -1,13 +1,20 @@
 package lists
 
+// #list
+
+// Reduce - Simplified version of foldLeft.
+
+// First argument is where result is passed
+// Second argument is always the current item in the collection.
+
 object ReduceExamples extends App {
 
-  val sum1 = List(1,2,3).reduceLeft{ (left, right) => {
-                                       println("left:  " + left + " right: " + right)
-                                       left + right}
+  val sum1 = List(1,2,3,4,5).reduceLeft{ (total, current) => {
+                                       println("total:  " + total + " current: " + current)
+                                       total + current}
                                    }
 
-  val sum2 = List(1,2,3) reduce(_ + _)  // short version
+  val sum2 = List(1,2,3,4,5) reduce(_ + _)  // short version
 
 
   println (sum1)  // 6 (1+2+3 = 6)
