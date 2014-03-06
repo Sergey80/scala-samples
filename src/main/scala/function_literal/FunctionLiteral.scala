52768b185c004d934af8ce7d49a7029f29edb512
Sergey80 <ses.box@gmail.com>
2014-03-05 22:08:43 -0500

    #zipWithIndex - added delete() function to CRUD example
    little fix

--- src/main/scala/function_literal/FunctionLiteral.scala
+++ src/main/scala/function_literal/FunctionLiteral.scala
@@ -14,22 +14,22 @@ object FunctionLiteral extends App {
 
 
 
-  // And this is anonymous function - there is no name of this function - you see, no name = anonymous:
+  // And this is #anonymous-function - there is no name of this function - you see, no name = anonymous:
 
   (a: Int) => a + 1 // so, yes - this is an instance of class Function1
 
 
 
-  // Sometimes though, we want to make use of anonymous function having a reference to it
+  // Sometimes though, we want to make use of #anonymous-function having a reference to it
      // then it is not anonymous anymore.. I know but let's say we don't know its name but only nick name
 
-  // Here are we go.. when it comes to referencing anonymous function - #Function-Literal appears
+  // Here are we go.. when it comes to referencing #anonymous-function - #Function-Literal appears
 
   // #Function-Literal is about syntax sugar, is an alternative form how we can define a function:
   val v = (a: Int) => a + 1 // v: Int => Int = <function1>
   def f = (a: Int) => a + 1 // f: Int => Int
 
-    // This reference 'v' is - #Function Value because it represents a value (instance) of Function1 instance
+    // This reference 'v' is - #Function-Value because it represents a value (instance) of Function1 instance
 
     // ( So, the notion of #Function-Literal
         // is somewhere in-between two notions: #Anonymous-Function & #Function-Value )
@@ -55,7 +55,7 @@ object FunctionLiteral extends App {
   }
 
   // A: Not there are not - because, remember #Function-Literal is about syntax (sugar)
-  // - lightweight syntax for defining anonymous functions
+  // - lightweight syntax for defining #anonymous-functions
 
   // But technically v1 is the same as v2, and f1 is same as f2
 
