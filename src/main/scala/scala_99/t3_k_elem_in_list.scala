a0e374532491970a600bbd755bf3f26cdef90b41
Sergey <ses.box@gmail.com>
2014-04-03 22:09:31 -0400

    Task 3 - Find the Kth element of a list.
    [*]

--- src/main/scala/scala_99/t3_k_elem_in_list.scala
+++ src/main/scala/scala_99/t3_k_elem_in_list.scala
@@ -1,11 +1,22 @@
 package scala_99
 
+// Find the Kth element of a list.
+
+// #list #loop #stable-identifier
 object t3_k_elem_in_list extends App {
 
-//  def nth(k:Int, list:List[Int]): Int = list match {
-//
-//  }
+  def nth(k:Int, list:List[Int]): Int =  {
 
-//  nth(2, List(1, 1, 2, 3, 5, 8)) // 2
+    def loopToK(list:List[Int], i:Int = 0): Int = i match {
+      case `k` => list.head           // #stable-identifier
+      case _ => loopToK(list.tail, i+1)
+    }
+
+    loopToK(list)
+
+  }
+
+
+  println ( nth(2, List(1, 1, 2, 3, 5, 8)) ) // 2
 
 }
