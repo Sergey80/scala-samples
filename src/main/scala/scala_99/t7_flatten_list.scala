8d3eb0dbd2112b597164f35f04b9e21234458bf1
Sergey80 <ses.box@gmail.com>
2014-05-05 13:49:42 -0400

    scala 99 - flatten
    fixed / improved

--- src/main/scala/scala_99/t7_flatten_list.scala
+++ src/main/scala/scala_99/t7_flatten_list.scala
@@ -3,14 +3,14 @@ package scala_99
 
 object t7_flatten_list extends App {
 
-  def flatten2(xs: List[Any]): List[Any] = xs match {
+  def flatten(xs: List[Any]): List[Any] = xs match {
     case Nil => Nil
-    case (head: List[_]) :: tail => flatten2(head) ++ flatten2(tail)
-    case head :: tail => head :: flatten2(tail)
+    case (head: List[_]) :: tail => flatten(head) ++ flatten(tail)
+    case head :: tail => head :: flatten(tail)
   }
 
-  println( flatten2(List(List(0, List(1,2)), 3, List(4, List(5, 6)))) )
-  println ( flatten2 ( List(List(List(1))) ) )
+  println( flatten  (List(List(0, List(1,2)), 3, List(4, List(5, 6)))) )
+  println ( flatten ( List(List(List(1))) ) )
 
 
 }
