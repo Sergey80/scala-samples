6967749eee90871f1aec1dc7ac71d619394c11b3
Sergey80 <ses.box@gmail.com>
2014-05-13 22:34:06 -0400

    scala 99 problems.  7th - flatten. added comments and 2nd impl

--- src/main/scala/scala_99/t7_flatten_list.scala
+++ src/main/scala/scala_99/t7_flatten_list.scala
@@ -3,14 +3,33 @@ package scala_99
 
 object t7_flatten_list extends App {
 
-  def flatten(xs: List[Any]): List[Any] = xs match {
-    case Nil => Nil
-    case (head: List[_]) :: tail => flatten(head) ++ flatten(tail)
-    case head :: tail => head :: flatten(tail)
+
+  {  // pure recursion
+
+    def flatten(xs: List[Any]): List[Any] = xs match {
+      case Nil => Nil
+      case (head_list: List[Any]) :: tail => flatten(head_list) ::: flatten(tail)
+      case head_elem :: tail => head_elem :: flatten(tail)    // looping through the list
+    }
+
+    val list = List(1, List(2, 3), 4)
+    println(list + " -> " + flatten(list))
+
   }
 
-  println( flatten  (List(List(0, List(1,2)), 3, List(4, List(5, 6)))) )
-  println ( flatten ( List(List(List(1))) ) )
+  { // recursion + accumulation
+
+    def flatten(xs: List[Any], acc:List[Any] = List()): List[Any] = xs match {
+      case Nil => Nil
+      case (head_list: List[Any]) :: tail   =>  flatten(head_list) ::: acc   // adding result to the list
+      case head_elem :: tail                =>  head_elem :: flatten(tail) // looping through the list
+    }
+
+    val list = List(1, List(2, 3), 4)
+    println(list + " -> " + flatten(list))
+
+
+  }
 
 
 }
