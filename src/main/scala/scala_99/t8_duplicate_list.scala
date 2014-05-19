92c7e5cd9fb01ef041eb5f46ef012fbbc2c0f6b6
Sergey80 <ses.box@gmail.com>
2014-05-19 15:12:44 -0400

    P08 - Eliminate consecutive duplicates of list elements.
    added fold implementation

--- src/main/scala/scala_99/t8_duplicate_list.scala
+++ src/main/scala/scala_99/t8_duplicate_list.scala
@@ -34,4 +34,29 @@ object t8_duplicate_list extends App {
   }
 
 
+  // 3. using fold
+  {
+
+   def compress[T](list:List[T]): List[T] =
+     list.foldRight(List[T]()) { // will be adding tot he empty list
+      (right, result) => if (result.isEmpty || result.head != right) right :: result
+                         else result
+     }
+
+    // just reminder that we may use 'case'
+    def compress2[T](list:List[T]): List[T] =
+      list.foldRight(List[T]()) { // will be adding to the empty list
+        case (right, Nil)                              => List(right)
+        case (right, result) if (result.head != right) => right :: result
+        case (_,     result)                           => result
+      }
+
+
+    val result1 = compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    val result2 = compress2(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    println (result1)
+    println (result2)
+
+  }
+
 }
