a313d1db79a183a0d584aafcc40331ec6b681349
Sergey80 <ses.box@gmail.com>
2014-10-01 09:02:46 -0400

    list-comprehension example. comparing to Haskell implementation

---
+++ src/main/scala/looping/listComprehension.scala
@@ -0,0 +1,26 @@
+// #list-comprehension #haskell
+
+package looping
+
+/*
+   While in Haskell:
+
+   contact :: [[a]] -> [a]
+   contact xss = [x | xs <- xss, x <- xs]
+   result = contact [[1],[3,4,5],[6]]       -- [1,2,3,4,5]
+
+*/
+
+object ListComprehension extends App {
+
+  def contact(xss: List[List[Int]]) : List[Int] = for {
+      xs <- xss
+      x <- xs
+  } yield x
+
+
+  val result = contact( List( List(1), List(3,4,5), List(6)) )
+
+  println("result: " + result)      // result: List(1, 3, 4, 5, 6)
+
+}
