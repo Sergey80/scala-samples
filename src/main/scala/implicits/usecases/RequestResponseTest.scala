32b21daa44fd2100f13404dc4dae1f647cdf8a05
Sergey80 <ses.box@gmail.com>
2015-02-15 18:15:13 -0500

    fixing the logic

--- src/main/scala/implicits/usecases/RequestResponseTest.scala
+++ src/main/scala/implicits/usecases/RequestResponseTest.scala
@@ -1,7 +1,6 @@
 package implicits.usecases
 
 object RequestResponseTest extends App {
-
   type User = String      // for simplicity sake - all are strings
   type Request = String
   type Result = String
@@ -13,27 +12,34 @@ object RequestResponseTest extends App {
   var userDB = List("user1", "user2", "user3") // our simple database
 
   val user = "user1"  // usually user available from the session
-  var request:Request = "request"  // current request
+  var request : Request = "request"  // current request
 
-
-  // wraps request into Action
+  // check authorization and wraps request into Action
   def withAuth(f: User => (Request => Result) ): Action = {
-
     println("withAuth in play...")
 
-    val result:Result = f("user1")(request) // is user found or not
+    val result:Result = f("user1")(request) // (is user was found or not)
 
-    if (result == "ok")  { println("wrapped to Action as authorized"); OkAction("") }
-      else {
-        println("wrapped to Action as not authorized")
-        UnauthorizedAction("")
-      }
+    def isAuthorized(user:User): Boolean = {
+      println("check authorisation...")
+      true                                // authorize everyone, welcome :)
+    }
 
+    def onUnAuthorized(request: Request): Action = {
+      println("wrapped to Action as not authorized")
+      UnauthorizedAction(request)
+    }
+
+    if (result == "ok") {
+      if (isAuthorized(request)) {
+        println("wrapped to Action as authorized")
+        OkAction(request)
+      } else onUnAuthorized(request)
+    } else onUnAuthorized(request)
   }
 
   //
   def withUser(f: User => (Request => Result)) : Action = {
-
     println("withUser in play...")
 
     def findInDb(user: User): Option[User] = {
@@ -41,57 +47,53 @@ object RequestResponseTest extends App {
     }
 
     val authResult:Action = withAuth (    // 'withUser' requires help of 'withAuth'
-
-      // Create anonymous function to please the 'withAuth'
-      // this function already knows who is user, what request is,
-      // but doesn't what Result is.
-      // In our case (since we are in 'withUser') the Result is the
-      //  fact whether the user exists in users database.
-      // So it tries to get the Result by searching for the current user.
-      // If user has found:
-      //  then it makes sense to continue with Authorisation (withAuth)
-      // If not:
-      //  then ...
+      /* Create anonymous function to please the 'withAuth'
+         this function already knows who is user, what request is,
+         but doesn't what Result is.
+         In our case (since we are in 'withUser') the Result is the
+         fact whether the user exists in users database or not.
+         So it tries to get the Result by searching for the current user.
+         If user has been found then it makes sense to
+         continue with Authorisation (withAuth) */
       user => request => {
-
-        val userOption = findInDb("user1") // find the user in users db
-
-        val result:Result = userOption match {
-          case Some(_) => {                         // user found
-            println("user has been found");         //
-            f(user)(request)                        // do work
-            "ok"                                    //
+        val userOption = findInDb("user1")          // find the user in users db
+        val result:Result = userOption match {      // check if user exists
+          case Some(_) => {                         // user has been found
+            println("user has been found")
+            "ok"
           }
-          case None    => {                         // user not found
-            println("user has not been found"); "not ok"
+          case None    => {                         // user has not been found
+            println("user has not been found")
+            "not ok"
           }
         }
-
-        result // "ok" / "not ok"
-
+       result // "ok" / "not ok" (user has been found or not)
       }
     )
-    authResult  // OkAction / NotOkAction
+
+    authResult match {
+      case OkAction(_) => f(user)(request)  // if authorized do The work
+    }
+
+    authResult
   }
 
   // Let's run this and to some work
-
   def doWork() = withUser {                     // doWork -> withUser -> withAuth  (like Decorator/Wrapper pattern)
     user => request => {
       // if user exists (withUser) and authorization is ok (withAuth), then this work will done
-      println("do some important work here")
+      println("do some important work here!!")
       "work is done!"                           // Result is just a String
     }
   }
-
   val result = doWork()  // doWork doesn't care about user or request
 }
-
-// Output:
-/*
+/* Output:
 withUser in play...
 withAuth in play...
 user has been found
-do some important work here
+check authorisation...
 wrapped to Action as authorized
+do some important work here!!
+
 */
