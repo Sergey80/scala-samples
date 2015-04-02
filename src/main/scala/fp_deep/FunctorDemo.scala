76b97f7ce3adaf6554eb5b0df173cfab104655d2
Sergey80 <ses.box@gmail.com>
2015-04-01 22:20:09 -0400

    functor - added comments

--- src/main/scala/fp_deep/FunctorDemo.scala
+++ src/main/scala/fp_deep/FunctorDemo.scala
@@ -1,34 +1,39 @@
 package fp_deep
 
 
-//TODO: add comments
+// #functor #higher-kinded-type #yype-constructor
+
+import scala.language.higherKinds  // if not set/imported - the warning about "higher-kinded type' will be generated
 
 object FunctorDemo extends App {
 
-  trait Functor[F[_]] {
-    def fmap[A, B](f: A => B): F[A] => F[B]
+  trait Functor[T[_]] {  // T[_] - Type Constructor that takes one type parameter. ("higher-kinded type" )
+    def fmap[A, B](f: A => B): T[A] => T[B] // takes function from A=>B and returns wrapped T[A] => T[B]
   }
 
-  implicit val listFunctor = new Functor[List] {
-    def fmap[A, B](f: A => B): List[A] => List[B] = {
-      case Nil => Nil
-      case a :: as => f(a) :: fmap(f)(as)
+  val listFunctor = new Functor[List] {      // List - type constructor
+    def fmap[A, B](f: A => B): List[A] => List[B] = {   // [A], [B] - type parameters for that type constructor
+      case Nil     => Nil
+      case a :: as => f(a) :: fmap(f)(as)   // List[B]
     }
   }
 
-  implicit val optionFunctor = new Functor[Option] {
-    def fmap[A, B](f: A => B): Option[A] => Option[B] = {
-      case None => None
+  val optionFunctor = new Functor[Option] {  // Option - type constructor
+    def fmap[A, B](f: A => B): Option[A] => Option[B] = { // [A], [B] - type parameters for that type constructor
+      case None    => None
       case Some(a) => Some(f(a))
     }
   }
 
+  // just reminding:
+  // case x => y    is same as (f: X => Y) - the function that takes value of type X and returns value of type Y
+  // so, by these 'case's we return a function T[A] -> T[B] as was specified in trait
 
-  val result1 = listFunctor.fmap[Int,Int](_ + 1)(List(1,2,3))
-  val result2 = optionFunctor.fmap[Int,Int](_ + 1)(Some(1))
+  val result1 = listFunctor.fmap[Int,Int]  (_ + 1) ( List(1,2,3) )    // have to specify [Int,Int] - scala/jvm limitation (comparing to Haskell)
+  val result2 = optionFunctor.fmap[Int,Int](_ + 1) ( Some(1) )
 
-  println(result1)
-  println(result2)
+  println (result1)    // List(2,3,4)
+  println (result2)    // List(2)
 
 
 }
