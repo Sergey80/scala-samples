946ffa11e5ccfd0de476cab50b128c25ea686d17
Sergii Starodubtsev <ses.box@gmail.com>
2015-08-19 21:36:31 -0400

    Concurrency. Future. Thread pool. Deadlock

---
+++ src/main/scala/concurency/DeadLockSample1.scala
@@ -0,0 +1,23 @@
+package concurency
+
+import java.util.concurrent.Executors
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ExecutionContext, Await, Future}
+
+// http://stackoverflow.com/questions/32107833/scala-future-deadlock-fixed-4-thread-pool
+
+object DeadLockSample1 {
+
+    def main(args: Array[String]) {
+
+      implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(4))
+
+      val futures = List( Future{1} )
+
+      val result: Future[List[Int]] = Future.sequence(futures)
+
+      Await.ready(result, Duration.Inf)
+
+    }
+
+}
