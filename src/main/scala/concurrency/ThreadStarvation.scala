6e2531cb18dab34d0317bb9c0c98165d7f9c01cc
Sergii Starodubtsev <ses.box@gmail.com>
2015-08-22 09:29:35 -0400

    Thread starvation example

--- src/main/scala/concurrency/ThreadStarvation.scala
+++ src/main/scala/concurrency/ThreadStarvation.scala
@@ -1,41 +1,82 @@
 package concurrency
-
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future, blocking}
-
 import org.scalameter._
+
+// #thread #starvation #futures
 
 object ThreadStarvation extends App {
 
+  /*
+      default/ global Ex Context (thread pool) by default deals with n default active threads,
+      were n is amount of cores on particular machine
+   */
   implicit val ec = scala.concurrent.ExecutionContext.Implicits.global
 
+  val cores = Runtime.getRuntime.availableProcessors
+
+  // --- 1 ---
+
   val time1 = measure {
-    
-    val works = (1 to 8) map { x =>
+
+    // creates amount of threads / futures in 2 times more than ExContext may handle at a time
+
+    val works = (1 to cores * 2) map { x =>
       Future {
-        Thread.sleep(1000)
+        Thread.sleep(1000) // each tread takes 1 second to run
         println(s"work $x is done")
       }
     }
-    Await.ready(Future.sequence(works), Duration.Inf)
+    Await.ready(Future.sequence(works), Duration.Inf) // half of all threads will be execution in parallel (another half will be waiting for its turn)
   }
+
+  // as a result the execution time will take 2 seconds
 
   println("running time1: " + time1.toFloat / 1000F + " sec") // 2 sec
 
-  //
+  // --- 2 ---
 
   val time2 = measure {
 
-    val works = (1 to 8) map { x =>
+    val works = (1 to cores * 2) map { x =>
         Future {
-          blocking {                    // !
-            Thread.sleep(1000)
-            println(s"work $x is done")
+          blocking {                    // ! here we believe that the execution may take time
+            Thread.sleep(1000)                // so we marking this execution with 'blocking'
+            println(s"work $x is done")         // to give a hint for Ex Context to create new addition threads if needed
           }
         }
     }
     Await.ready(Future.sequence(works), Duration.Inf)
   }
 
+  // as a result, the execution time will take ~1 sec
+    // since all threads will be executing in max "cores * 2" threads -
+    // none of them will wait for the free space in the poll to execute
+
   println("running time2: " + time2.toFloat / 1000F + " sec") // 1 sec
+
+
+  // --- 3 ---
+
+  // of course if we have much more works to do),
+  // then even if we use 'blocking'
+
+  val time3 = measure {
+
+    val works = (1 to cores * 10000) map { x =>   // 10000 more than cores!
+      Future {
+        blocking {                   // !
+          Thread.sleep(1000)
+          println(s"work $x is done")
+        }
+      }
+    }
+    Await.ready(Future.sequence(works), Duration.Inf)
+  }
+
+  println("running time3: " + time3.toFloat / 1000F + " sec") // 10.89 sec. Not bad at all
+
+  // for 1000 workers it would take only 2.4 sec !
+
 }
+
