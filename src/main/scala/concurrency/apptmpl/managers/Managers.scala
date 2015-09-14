5cee0cc918c615da449dce0b498763f21111963c
Sergii Starodubtsev <ses.box@gmail.com>
2015-09-13 21:41:09 -0400

    little improvements

--- src/main/scala/concurrency/apptmpl/managers/Managers.scala
+++ src/main/scala/concurrency/apptmpl/managers/Managers.scala
@@ -1,12 +1,18 @@
 package concurrency.apptmpl.managers
 
-import java.util.concurrent.{Executor}
+import java.util.concurrent.{Executors, Executor}
 
 object Managers {
 
+  // 1.
   // same as 'global' ex context
-  implicit val waitingCtx = scala.concurrent.ExecutionContext.fromExecutor(null: Executor)
+  //implicit val waitingCtx = scala.concurrent.ExecutionContext.fromExecutor(null: Executor)
 
-  // fixed ex context
+  // 2.
   //implicit val waitingCtx = scala.concurrent.ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
+  implicit val waitingCtx = scala.concurrent.ExecutionContext.fromExecutor(Executors.newFixedThreadPool(40))
+
+  // 3.
+  // Creates a thread pool that creates new threads as needed, but will reuse previously constructed threads when they are available.
+  //implicit val waitingCtx = scala.concurrent.ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 }
--- src/main/scala/concurrency/apptmpl/services/Services.scala
+++ src/main/scala/concurrency/apptmpl/services/Services.scala
@@ -3,9 +3,8 @@ package concurrency.apptmpl.services
 import java.util.concurrent.{Executors, Executor}
 
 object Services {
+
    implicit val blockingExContext = scala.concurrent.ExecutionContext.fromExecutor(null: Executor)
 
-  // Creates a thread pool that creates new threads as needed, but will reuse previously constructed threads when they are available.
-  //implicit val blockingExContext = scala.concurrent.ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 }
 
