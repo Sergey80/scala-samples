849cc5fdb9e95308f6f140013d0b17d090469dc3
Sergii Starodubtsev <ses.box@gmail.com>
2015-09-24 23:08:46 -0400

    +overriding, simple stuff..

--- src/main/scala/overriding/OverridePlus.scala
+++ src/main/scala/overriding/OverridePlus.scala
@@ -25,7 +25,7 @@ object OverridePlus extends App {
 
   class B1 extends A1 {
     type ChildOfA = B              // concrete type
-    override def aa(b:B):B = new B          // ! not you CAN do it
+    override def aa(b:B):B = new B          // ! now you CAN do it
   }
   
   val result2 = (new B1).aa(new B)
