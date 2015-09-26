f6b50008701d39dc302cdcb74815a0a77c88e854
Sergii Starodubtsev <ses.box@gmail.com>
2015-09-26 16:18:22 -0400

    performance measurement, scala-meter sample. fix: typo

--- src/main/scala/concurrency/performance/MyBench.scala
+++ src/main/scala/concurrency/performance/MyBench.scala
@@ -2,9 +2,27 @@ package concurrency.performance
 
 import concurrency.logging.threadfactory.CustomThreadFactory
 import org.scalameter._
-
 import scala.concurrent.{Future, Await, blocking}
 import scala.concurrent.duration._
+
+// ForkJoinPool: https://en.wikipedia.org/wiki/Fork%E2%80%93join_mode
+
+// In this example we show how much schedulingEcCtx is involved
+// For example here we see the stats that shows that even though all work is done workPool / workerEcCtx
+
+// but the schedulingPool/schedulingEcCtx is used in the Future.sequence(statsF),
+// where it waits until all features have completed, and it also takes effort
+// we see that schedulingPool will use all cores and will use many "steals" (about 2383)
+
+// (https://en.wikipedia.org/wiki/Work_stealing)
+
+// stealing (work stealing scheduler) - reusing existing threads in the pool
+
+// "a virtue of employing work-stealing: all threads in the pool attempt to find and execute 'subtasks' created by other 'active' tasks (eventually blocking waiting for work if none exist)"
+
+// But if we change 'schedulingPool' size to have, say 40 threads,
+// then will see that actually it would use only 16 of them,
+// keep stealing factor about 100 steals
 
 object MyBench extends App {
 
@@ -14,16 +32,23 @@ object MyBench extends App {
   val schedulingPool = new scala.concurrent.forkjoin.ForkJoinPool(4, CustomThreadFactory("work"), CustomThreadFactory.uncaughtExceptionHandler, true)
   val schedulingEcCtx = scala.concurrent.ExecutionContext.fromExecutor(schedulingPool)
 
+  /**
+   * @param active  - The pool attempts to maintain enough active (or available) threads by dynamically adding, suspending, or resuming internal worker threads
+   * @param running - running threads/tasks (supposed to be max: 32767)
+   * @param steals  - amount of stealing tasks
+   * @param tasks   - number of tasks submitted to this pool
+   * @param submissions -  number of tasks submitted to this pool that have not yet begun executing
+   */
   case class Stat(active:Int, running:Int, steals:Long, tasks:Long, submissions:Int)
 
   var statPairs : Seq[(Stat, Stat)] = _
 
   val m = measure {
 
-    val statsF = (1 to 20000) map { n =>
+    val statsF = (1 to 20000) map { n => // a lot of work to do
       Future {
         blocking {
-          Thread.sleep(5000)
+          Thread.sleep(5000) // work takes some time
 
           Pair(
           
@@ -71,7 +96,8 @@ object MyBench extends App {
   println("Scheduling pool size: " + schedulingPool.getPoolSize) // should not be more than 4
 
 
-  //
+  // stats function
+
   def maxStat( statsPair:Seq[(Stat, Stat)]) (f: Seq[(Stat, Stat)] => Seq[Stat] ) :Stat = {
     val stats = f(statsPair)
     Stat (
